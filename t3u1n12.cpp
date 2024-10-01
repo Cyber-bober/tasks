0ff63120b0222dacd66168de55bb5cd15a3d377a
Vlados <bykovvlados@vk.com>
2024-10-01 21:57:46 +0400

    Add files via upload

---
+++ t3u1n12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main(){
+    int x, y;
+    cout << "x= ";
+    cin >> x;
+    cout << "y= "
+    cin >> y;
+    if (abs(x) < 40 || abs(y) < 40){
+        cout << "Да"
+    }
+    else if (abs(x) < 40 || abs(y) < 40){
+        cout << "На границе"
+    }
+    return 0;
+}
