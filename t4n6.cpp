02ded97e0658f8510523c5fc3bcae4ada18f1f5b
Vlados <bykovvlados@vk.com>
2024-11-05 21:53:46 +0400

    Add files via upload

---
+++ t4n6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    double b1 = 2, b;
+    int n;
+    cout << "n = "; cin >> n;
+    for (int i = 2; i <= n; i++)
+    {
+        b = 0.5*((1/b)+b);
+        b1 = b;
+    }
+    cout << "b" << n << " = " << b << endl;
+    return 0;
+}
