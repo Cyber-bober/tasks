1e9d3bbdf74769fe2b1734f163a153a7b2ed2846
Vlados <bykovvlados@vk.com>
2024-11-19 21:20:38 +0400

    Add files via upload

---
+++ t5u1n9.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double factorial(int num) {
+    double result = 1;
+    for (int i = 1; i <= num; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+double sumOfFactorials(int n) {
+    double sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum += factorial(i);
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    cout << "n: ";
+    cin >> n;
+    double result = sumOfFactorials(n);
+    cout << "s: " << result << endl;
+    return 0;
+}
