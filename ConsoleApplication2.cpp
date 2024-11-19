0463c0394f28c142667e0fff0d6dd9b1f86c8dd6
Vlados <bykovvlados@vk.com>
2024-11-19 21:52:42 +0400

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	double x, p = 1, b = 1;
+	cout << "x = ";
+	cin >> x;
+	cout << "n = ";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		b *= x;
+		p *= (1+)
+	}
+	cout << "p = " << p << endl;
+	return 0;
+}
