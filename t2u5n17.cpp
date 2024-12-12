41365bfe35655526c8334b08ea93e8c6f6a5f5cb
Vlados <bykovvlados@vk.com>
2024-12-12 19:39:21 +0400

    Update and rename ConsoleApplication2.cpp to t2u5n17.cpp

---
+++ t2u5n17.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	double x, p = 1;
+	cout << "x = ";
+	cin >> x;
+	double b = x;
+	cout << "i = ";
+	cin >> i;
+	for (int i; n <= i; n++)
+	{
+		b *= -pow(x,2);
+		p *= (1+ b / (pow(n, 3) + pow (n, 2)))
+	}
+	cout << "p = " << p << endl;
+	return 0;
+}
