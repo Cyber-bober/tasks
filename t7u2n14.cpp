5be2733d4ea83d6f8659c7366f1abddd8af363b3
Vlados <bykovvlados@vk.com>
2024-12-23 21:54:24 +0400

    Add files via upload

---
+++ t7u2n14.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string  s, s0;
+
+    cout << "Введите строку s: ";
+    getline(cin, s); // Считываем строку с учетом пробелов
+    cout << "Введите подстроку s0: ";
+    getline(cin, s0);
+
+    size_t pos; //Size_t - беззнаковый целочисленный тип
+    while ((pos = s.find(s0)) != string::npos) {  //npos - проверка до конца строки
+        s.erase(pos, s0.length();
+    }
+    cout << s;
+  
+    return 0;
+}
